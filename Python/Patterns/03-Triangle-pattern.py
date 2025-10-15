9f17b98223eec6afb0dacc1caee938af2701d9dd
AhmadAnsari-006 <ahmadshoaib0511@gmail.com>
2025-10-15 21:25:59 +0530

    new patterns

--- Python/Patterns/03-Triangle-pattern.py
+++ Python/Patterns/03-Triangle-pattern.py
@@ -1,9 +1,7 @@
 n = int(input("Enter length of pattern: "))
 for i in range(1, n+1):
-    # Print spaces
     for j in range(n - i):
         print(" ", end=" ")
-    # Print stars
     for k in range(i):
         print("*", end=" ")
     print()
